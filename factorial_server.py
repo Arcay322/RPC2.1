a0e839ae67358542aa52fd097bf4a73fd94b9dd1
Arnie <u22203827@utp.edu.pe>
2024-10-09 02:29:24 -0500

    RPC2.1 V2

--- factorial_server.py
+++ factorial_server.py
@@ -2,6 +2,8 @@ from concurrent import futures
 import grpc
 import factorial_pb2
 import factorial_pb2_grpc
+import os
+
 
 class FactorialService(factorial_pb2_grpc.FactorialServiceServicer):
     def Calculate(self, request, context):
@@ -11,13 +13,18 @@ class FactorialService(factorial_pb2_grpc.FactorialServiceServicer):
             result *= i
         return factorial_pb2.FactorialResponse(result=result)
 
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     factorial_pb2_grpc.add_FactorialServiceServicer_to_server(FactorialService(), server)
-    server.add_insecure_port('[::]:50051')
+
+    # Obtener el puerto asignado por Render o usar el 50051 como fallback
+    port = os.environ.get('PORT', '50051')
+    server.add_insecure_port(f'[::]:{port}')  # Escuchar en todas las interfaces
     server.start()
-    print("Servidor escuchando en el puerto 50051...")
+    print(f"Servidor escuchando en el puerto {port}...")
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
